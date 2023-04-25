859cfd9d2088768414a839014650217ee7d59577
Loren Cerri <lorencerri@gmail.com>
2023-04-24 20:04:58 -0400

    Turn documents into cards

---
+++ app/src/main/java/com/cerri/knowledgebasefinal/components/DocumentItem.kt
@@ -0,0 +1,2 @@
+package com.cerri.knowledgebasefinal.components
+
--- app/src/main/java/com/cerri/knowledgebasefinal/components/DocumentList.kt
+++ app/src/main/java/com/cerri/knowledgebasefinal/components/DocumentList.kt
@@ -1,30 +1,39 @@
 package com.cerri.knowledgebasefinal.components
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import com.cerri.knowledgebasefinal.Document
 
 @Composable
 fun DocumentList(documents: List<Document>) {
 
+    if (documents.isEmpty()) return LoadingIndicator()
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-
     ) {
-        if (documents.isEmpty()) {
-            CircularProgressIndicator()
-        }
 
         documents.forEach {
-            Text(text = "${it.title} - ${it.description}")
+            Card(modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .clickable {}) {
+                Column(modifier = Modifier.padding(15.dp)) {
+                    Text(text = it.title, fontWeight = FontWeight.Bold)
+                    Text(text = it.description)
+                }
+            }
         }
     }
 }
